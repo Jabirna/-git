bde695b6edde433568e3a792bae133dfec297605
jabir <jabirnadeeem@gmail.com>
2023-11-03 17:55:02 +0530

    SIZE Changes

--- 900 woods.js
+++ 900 woods.js
@@ -93,7 +93,15 @@ slider.addEventListener('mousemove', (e) => {
 
 
 
+        var x = document.getElementById("myAudio"); 
+              
+        function playAudio() { 
+          x.play(); 
+        } 
         
+        function pauseAudio() { 
+          x.pause(); 
+        } 
 
 
 
